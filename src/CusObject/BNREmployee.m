83b490b4cbef0e1ee0abc176f4f6b66e32deab90
joney <joney.s.li@newegg.com>
2023-08-31 13:16:27 +0800

    对象实例变量 和属性结合

--- src/CusObject/BNREmployee.m
+++ src/CusObject/BNREmployee.m
@@ -1,3 +1,4 @@
+
 //
 //  BNREmployee.m
 //  CusObject
@@ -10,13 +11,46 @@
 
 @implementation BNREmployee
 
-// description
-- (NSString *)description
+- (void)setAssets:(NSArray *)a
 {
-    return [NSString stringWithFormat:@"<Employee %u>",self.employeeID];
+    _assets = [a mutableCopy];
+}
+
+- (NSArray *)assets
+{
+    return [_assets copy];
+}
+
+- (void)addAssets:(BNRAsset *)a
+{
+    if(!_assets){
+        _assets = [[NSMutableArray alloc]init];
+    }
+    
+    [_assets addObject:a];
+}
+
+- (unsigned int)valueOfAssets
+{
+ //累加物品售价
+    unsigned int sum = 0;
+    for (BNRAsset *a in _assets) {
+        sum += [a resaleValue];
+    }
+    return sum;
 }
 
 
+// description
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"<Employee %u: %u in assets>",self.employeeID, self.valueOfAssets];
+}
+
+- (void)dealloc
+{
+    NSLog(@"deallog %@",self);
+}
 
 // overView
 - (float)bodyMassIdex
--- src/CusObject/main.m
+++ src/CusObject/main.m
@@ -6,67 +6,47 @@
 //
 
 #import <Foundation/Foundation.h>
-//#import "BNRPerson.h"
-//#import "BNRPerson2.h"
 #import "BNREmployee.h"
-#import "BNRStockHolding.h"
+#import "BNRAsset.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        /**
-         
-         BNRPerson *mky = [[BNRPerson alloc]init];
-         
-         [mky setH:1.8];
-         [mky setW:96];
-         
-         NSLog(@"H -> %.2f", [mky getH]);
-         NSLog(@"bmi ->%f", [mky bodyMassIndex]);
-         */
+        // 存多个 BNREmployee；
+        NSMutableArray *employee =[[NSMutableArray alloc]init];
         
-        /**
-         BNRStockHolding *s1 =[[BNRStockHolding alloc]init];
-         BNRStockHolding *s2 =[[BNRStockHolding alloc]init];
-         BNRStockHolding *s3 =[[BNRStockHolding alloc]init];
-         [s1 setCurrentPrice:2.30];
-         [s1 setCurrentPrice:4.50];
-         [s1 setNumberOfShares:40];
-         
-         [s2 setCurrentPrice:12.19];
-         [s2 setCurrentPrice:10.56];
-         [s2 setNumberOfShares:90];
-         
-         [s3 setCurrentPrice:45.10];
-         [s3 setCurrentPrice:49.51];
-         [s1 setNumberOfShares:210];
-     
-         
-         NSMutableArray *m = [[NSMutableArray alloc]init];
-         [m addObject:s1];
-         [m addObject:s2];
-         [m addObject:s3];
-         
-         BNRPerson2 *mky = [[BNRPerson2 alloc]init];
-         
-         mky.w = 96;
-         mky.h = 1.8;
-         
-         NSLog(@"H -> %.2f",mky.h);
-         NSLog(@"bmi ->%f", [mky bodyMassIdex]);
-         */
+        for (int i =0; i<10; i++) {
+            // 构建十个BNREmployee
+            BNREmployee *e = [[BNREmployee alloc]init];
+            e.w = 90 +i;
+            e.h = 1.8 - (i / 10.0);
+            e.employeeID = i;
+            
+            [employee addObject:e];
+        }
         
-        // 继承例子演示demo
-        BNREmployee *mk = [[BNREmployee alloc]init];
-        mk.w = 96;
-        mk.h = 1.8;
-        mk.employeeID = 12;
-        mk.hireDate = [NSDate dateWithNaturalLanguageString:@"Aug 2nd, 2023"];
+        // 创建十个BNRAsstes
+        for (int i =0; i <10; i++) {
+            // 构建BNRAsset
+            BNRAsset *as = [[BNRAsset alloc]init];
+            
+            // 创建lable
+            NSString *lable = [NSString stringWithFormat:@"Latop %d",i];
+            as.lable = lable;
+            as.resaleValue = 350 + i *17;
+            
+            //0-9 随机数
+            NSUInteger randInt = random() % [employee count];
+            // 随机取一个 然后给它把addAssets 加进去
+            BNREmployee *randEmployee = [employee objectAtIndex:randInt];
+            [randEmployee addAssets:as];
+        }
         
-        NSLog(@"Mike has a w of %d", mk.w);
-        NSLog(@"Mike BMI %f", [mk bodyMassIdex]);
-        NSLog(@"Mike Id %u hired on %@",mk.employeeID, mk.hireDate);
-        NSLog(@"BMI of %.2f for %.2f years", [mk bodyMassIdex], mk.yearOfEmployee);
-        NSLog(@"===================");
+        // 输出测试
+        NSLog(@"ems: %@",employee);
+        NSLog(@"Giving up ownership of on employee");
+        [employee removeObjectAtIndex:5];
+        employee =nil;
+        
         
     }
     return 0;
