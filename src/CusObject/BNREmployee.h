c9c4be7b4de62a4a919304919624b12943a6ea37
joney <joney.s.li@newegg.com>
2023-08-31 13:32:33 +0800

    类扩展(私有属性)

--- src/CusObject/BNREmployee.h
+++ src/CusObject/BNREmployee.h
@@ -13,11 +13,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface BNREmployee : BNRPerson2
 {
-    NSMutableArray *_assets;
 }
 
 @property (nonatomic) unsigned int employeeID;
-@property (nonatomic) unsigned int officeAlarmCode;
 @property (nonatomic) NSDate *hireDate;
 @property (nonatomic, copy) NSArray *assets;
 
